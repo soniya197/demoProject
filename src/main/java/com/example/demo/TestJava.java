db4a63a742e276aafa1e15ddb0181bfd990e7f61
soniya <112462979+sardarshaik-eGov@users.noreply.github.com>
2023-06-27 18:37:41 +0400

    method added in testjava class

--- src/main/java/com/example/demo/TestJava.java
+++ src/main/java/com/example/demo/TestJava.java
@@ -7,4 +7,14 @@ public class TestJava {
 
 	}
 
+	public static int useRecursion(String someString, int i, int index) {
+
+		if (index >= someString.length()) {
+			return 1;
+		}
+
+		int count = someString.charAt(index) == i ? 1 : 0;
+		return count + useRecursion(someString, i, index + 1);
+
+	}
 }
